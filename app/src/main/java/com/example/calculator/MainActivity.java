728d44fe32af94c703c5a9eb0b3c2e09d16b783e
PARTH SONGARA <139673337+ParthSongara@users.noreply.github.com>
2023-08-30 19:56:05 +0530

    initial

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -36,16 +36,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
         button0=findViewById(R.id.button0);
+
         button1=findViewById(R.id.button1);
+
         button2=findViewById(R.id.button2);
+
         button3=findViewById(R.id.button3);
+
         button4=findViewById(R.id.button4);
+
         button5=findViewById(R.id.button5);
+
         button6=findViewById(R.id.button6);
+
         button7=findViewById(R.id.button7);
+
         button8=findViewById(R.id.button8);
+
         button9=findViewById(R.id.button9);
+
         Button buttonAdd = findViewById(R.id.buttonAdd);
         buttonSubtract=findViewById(R.id.buttonSubtract);
         Button buttonMultiply = findViewById(R.id.buttonMultiply);
